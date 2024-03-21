023241eb1e86be5593e9a12f59ee5d8a61b4247c
dayul <dyulchoo@naver.com>
2024-03-21 11:57:00 +0900

    02-01 이름공간(namespace)

--- 2024-CPP/02-01 기본예제.cpp
+++ 2024-CPP/02-01 기본예제.cpp
@@ -1,10 +1,13 @@
 #include <iostream> 
 
+// 이름 공간, std::를 안 해도 됨
+using namespace std;
+
 int main(void) {
 
 	// cout : 출력객체
 	// endl : 개행객체 (\n)
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << endl;
 
 	return 0;
 }
