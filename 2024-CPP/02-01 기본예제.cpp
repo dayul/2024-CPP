474c1480482b670b551f1914638abb940008d070
dayul <dyulchoo@naver.com>
2024-03-21 11:45:30 +0900

    02-01 C++ 기본예제

---
+++ 2024-CPP/02-01 기본예제.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+int main(void) {
+
+	// cout : Ãâ·Â°´Ã¼
+	// endl : °³Çà°´Ã¼ (\n)
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+}
--- 2024-CPP/example.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> 
-
-int main(void) {
-	printf("Hello World");		// 주석 테스트
-
-	return 0;
-}
