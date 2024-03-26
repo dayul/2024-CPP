ecc08df6ab1e5feb028bce42d138814c1399fc14
dayul <dyulchoo@naver.com>
2024-03-26 16:10:03 +0900

    주소값이 같음

--- 2024-CPP/grammar/배열과 포인터.cpp
+++ 2024-CPP/grammar/배열과 포인터.cpp
@@ -9,7 +9,13 @@ int main(void) {
 	char str[4] = "abc";
 
 	printf("%d %d\n", &arr, &arr[1]);		// 주소값 4byte 만큼의 차이
+	printf("%d %d\n", arr + 0, arr + 1);	// 주소값이 같음
+
+	printf("%d %d\n", arr[0], arr[1]);		// 같음
+	printf("%d %d\n", *(arr+0), *(arr+1));	// 포인터가 배열에 접근 가능
+
 	printf("%d %d\n", &str, &str[1]);		// 주소값 1byte 만큼의 차이
+
 	
 	return 0;
 }
