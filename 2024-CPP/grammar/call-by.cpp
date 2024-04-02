32b21258ed0a6ae9b1200d1a495a6ea094a96a6c
dayul <dyulchoo@naver.com>
2024-04-02 15:46:39 +0900

    call by reference

--- 2024-CPP/grammar/call-by.cpp
+++ 2024-CPP/grammar/call-by.cpp
@@ -7,10 +7,18 @@ void call_by_pointer(int* val) {
 	*val = 10;
 }
 
+void call_by_reference(int& val) {
+	// 참조 변수 val를 만들고 참조하는 num의 값이 변경됨
+	val = 11;
+}
+
 int main(void) {
 
 	int num = 4;		
-	call_by_pointer(&num);		// num의 주소값을 넘김, 값이 바뀜
+	call_by_pointer(&num);		// 포인터, num의 주소값을 넘김, 값이 바뀜
+	cout << num << endl;
+
+	call_by_reference(num);		// 참조
 	cout << num;
 	
 	return 0;
