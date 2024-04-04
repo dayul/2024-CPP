4761b8736dc49869775a8c1b8bd9f1e5242d4275
dayul <dyulchoo@naver.com>
2024-04-04 11:42:12 +0900

    String 대입 (C언어 방식2)

--- 2024-CPP/grammar/02-03 string대입.cpp
+++ 2024-CPP/grammar/02-03 string대입.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
 
 	char str[30] = "develop hungry";
 	char str2[30];
 
-	// str에 있는 내용을 str2에 복사
-	int i = 0;
-	for (; str[i] != '\0'; i++) {
-		str2[i] = str[i];
-	}
-	str2[i] = str[i];	// 널문자('\0') 복사
+	// 1. str에 있는 내용을 str2에 복사
+	//int i = 0;
+	//for (; str[i] != '\0'; i++) {
+	//	str2[i] = str[i];
+	//}
+	//str2[i] = str[i];	// 널문자('\0') 복사
+
+
+	// 2. str에 있는 내용을 str2에 복사
+	strcpy(str2, str);
 
 	printf("%s", str2);
 
