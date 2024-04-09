e4d5d421f9d8969d2c103b62e98784f0a842da32
dayul <dyulchoo@naver.com>
2024-04-09 15:42:44 +0900

    클래스는 default가 private

--- 2024-CPP/grammar/03-01 구조체와 클래스.cpp
+++ 2024-CPP/grammar/03-01 구조체와 클래스.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
-struct Student {
+class Student {
 	int hakbun_;
 	string name_;
 };
 
 int main(void) {
 	
-	struct Student dayul;
+	Student dayul;		// C++에서는 struct 키워드 빼도 됨
 	// 구조체는 default가 public
+	// 클래스는 default가 private
 	dayul.hakbun_ = 2215;
 	dayul.name_ = "추다율";
 
