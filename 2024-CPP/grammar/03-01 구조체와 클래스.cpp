9df7966f84f6dfb1d945f83aab7a09ed86537427
dayul <dyulchoo@naver.com>
2024-04-11 11:18:24 +0900

    정적할당과 동적할당

--- 2024-CPP/grammar/03-01 구조체와 클래스.cpp
+++ 2024-CPP/grammar/03-01 구조체와 클래스.cpp
@@ -29,11 +29,17 @@ public :
 
 int main(void) {
 	
-	Student dayul = Student(2215, "추다율");
+	// 동적 할당 : 메모리는 heap에서 할당, 실행(런타임) 시 메모리 크기가 정해짐
+	Student *dayul = new Student(2215, "추다율");
+	
+	// 정적 할당 : 메모리는 stack에서 할당, 컴파일 시 메모리 크기가 정해짐
 	Student stu = Student();
 
-	dayul.show();
+	(*dayul).show();
 	stu.show();
+
+	// 동적 할당 해제
+	delete dayul;
 
 	return 0;
 }
