1931ffa96cca98c7b41553a95b3091bae618036d
dayul <dyulchoo@naver.com>
2024-04-18 11:40:44 +0900

    03-02 생성자, 소멸자 호출시기 확인

--- 2024-CPP/grammar/03-02 생성자와 소멸자.cpp
+++ 2024-CPP/grammar/03-02 생성자와 소멸자.cpp
@@ -16,11 +16,13 @@ public :
 		name_ = new char[length + 1];		// '/0'가 들어가기 위한 공간 1개 더 추가
 		
 		strcpy(name_, name);
+		cout << "생성자 호출 완료" << endl;
 	}
 
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
 	~Student(void) {
 		delete[] name_;
+		cout << "소멸자 호출 완료" << endl;
 	}
 	
 	// 클래스의 멤버를 출력
@@ -33,9 +35,10 @@ public :
 int main(void) {
 	
 	Student* stu = new Student(2215, "추다율");
-	stu->show();
+	cout << "나는 아직도 배고프다 - 거스 히딩크" << endl;
 
-	delete stu;		// stu가 가르키는 객체만 소멸, name은 사라지지 않음 (생성자에서 동적할당 했을 경우)
+	delete stu;		
+	cout << "It ain't over till it's over - 요기베라" << endl;
 
 	return 0;
 }
