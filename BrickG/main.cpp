18c319cb70cbabf3aca35dbdf9135c2ffbbd7fd2
dayul <dyulchoo@naver.com>
2024-06-25 16:08:50 +0900

    공(ball) 클래스 구현

--- BrickG/main.cpp
+++ BrickG/main.cpp
@@ -1,5 +1,36 @@
 #include <SFML/Graphics.hpp>
 
+// Ball 클래스 정의
+class Ball {
+public:
+    sf::CircleShape shape;
+    sf::Vector2f velocity;
+
+    // 생성자
+    Ball(float mX, float mY) {
+        shape.setPosition(mX, mY);      // 위치
+        shape.setRadius(10.f);          // 크기
+        shape.setFillColor(sf::Color::Red);
+        shape.setOrigin(10.f, 10.f);
+        velocity = { -8.f, -8.f };      // 왼쪽으로, 위로 (속도)
+    }
+
+    void update() {
+        shape.move(velocity);
+
+        if (left() < 0) velocity.x = 8.f;
+        else if (right() > 800) velocity.x = -8.f;
+
+        if (top() < 0) velocity.y = 8.f;
+        else if (bottom() > 600) velocity.y = -8.f;
+    }
+
+    float left() { return shape.getPosition().x - shape.getRadius(); }
+    float right() { return shape.getPosition().x + shape.getRadius(); }
+    float top() { return shape.getPosition().y - shape.getRadius(); }
+    float bottom() { return shape.getPosition().y + shape.getRadius(); }
+};
+
 int main()
 {
     // init
