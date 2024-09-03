cda9cb5940b78cac5299f4cdefaa5ca88be8ff18
dayul <dyulchoo@naver.com>
2024-09-03 13:57:13 +0900

    다형성 적용 전

--- 2024-CPP/grammar/다형성 예제.cpp
+++ 2024-CPP/grammar/다형성 예제.cpp
@@ -9,6 +9,13 @@ public:
 		: civil_(civil), force_(force), name_(name), territory_(territory) {}
 	virtual ~Food() {}
 
+	void show() {
+		cout << "±¹¹Î ¼ö : " << civil_ << endl;
+		cout << "±º·Â : " << force_ << endl;
+		cout << "ÀÌ¸§ : " << name_ << endl;
+		cout << "¸éÀû : " << territory_ << endl;
+	}
+
 private:
 	int civil_;			// ±¹¹Î ¼ö
 	int force_;			// ±º·Â
@@ -22,6 +29,11 @@ public:
 		: Food(civil, force, name, territory), garlic_(garlic), pepper_(pepper) {}
 	virtual ~Kimchi() {}
 
+	void show() {
+		cout << "¸¶´Ã : " << garlic_ << endl;
+		cout << "°íÃß : " << pepper_ << endl;
+	}
+
 private:
 	int garlic_;
 	int pepper_;
@@ -32,6 +44,11 @@ public:
 	Jelly(int civil, int force, string name, int territory, int gelatin, int sugar) 
 		: Food(civil, force, name, territory), gelatin_(gelatin), sugar_(sugar) {}
 	virtual ~Jelly() {}
+
+	void show() {
+		cout << "Á©¶óÆ¾ : " << gelatin_ << endl;
+		cout << "¼³ÅÁ : " << sugar_ << endl;
+	}
 
 private:
 	int gelatin_;
@@ -44,7 +61,25 @@ public:
 		: Food(civil, force, name, territory), milk_(milk), rennet_(rennet) {}
 	virtual ~Cheese() {}
 
+	void show() {
+		cout << "¿ìÀ¯ : " << milk_ << endl;
+		cout << "ÀÀ°íÁ¦ : " << rennet_ << endl;
+	}
+
 private:
 	int milk_;
 	int rennet_;
 };
+
+int main(void) {
+	Food* player = new Kimchi(15, 95, "°«±èÄ¡", 86, 100, 100);
+	Food* friends = new Cheese(100, 20, "Â¥°èÄ¡", 100, 20, 100);
+	player->show();
+	cout << endl << endl;
+	friends->show();
+
+
+	delete friends;
+	delete player;
+	return 0;
+}
