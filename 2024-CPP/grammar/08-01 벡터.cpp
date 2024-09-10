1f26c0d1c369efcf0f5c9c7c50bafe7d4dbbc904
dayul <dyulchoo@naver.com>
2024-09-10 15:01:05 +0900

    08-01 벡터 개념

---
+++ 2024-CPP/grammar/08-01 벡터.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class example {
+	T data;
+};
+
+int main(void) {
+
+	vector<int> v;
+
+	// 벡터 추가
+	v.push_back(4);
+	v.push_back(6);
+	v.push_back(342);
+	v.push_back(43);
+	v.push_back(1000);
+
+	// 값 접근
+	cout << v[0] << endl;
+	cout << v.at(1) << endl;
+
+	// 값 수정
+	cout << v[4] << endl;
+	v[4] = 1001;
+	cout << v[4] << endl;
+
+	cout << "---------------" << endl;
+
+	// 크기 확인
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+	cout << "제거 전 크기 : " << v.size() << endl;
+
+	cout << "---------------" << endl;
+	
+	// 끝값 제거
+	v.pop_back();
+	v.pop_back();
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+	cout << "제거 후 크기 : " << v.size() << endl;
+
+	return 0;
+}
