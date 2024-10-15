734d2f46167ca9848eeb60fac85f1b9730b1af94
dayul <dyulchoo@naver.com>
2024-10-15 15:10:58 +0900

    auto 적용

--- 2024-CPP/grammar/08-02 맵.cpp
+++ 2024-CPP/grammar/08-02 맵.cpp
@@ -14,11 +14,16 @@ int main(void) {
 	// 맵은 순서가 없음
 	// first : Key
 	// seconde : Value
-	map<string, long long>::iterator iter;
-	for (iter = money.begin(); iter != money.end(); iter++) {
-		// 포인터이므로 ->
-		cout << iter->first << "는" << (*iter).second << endl;
+	//map<string, long long>::iterator iter;
+	//for (iter = money.begin(); iter != money.end(); iter++) {
+	//	// 포인터이므로 ->
+	//	cout << iter->first << "는" << (*iter).second << endl;
+	//}
+
+	for (auto iter = money.begin(); iter != money.end(); iter++) {
+		cout << iter->first << "는" << iter->second << endl;
 	}
+
 
 	// 값 수정
 	money["su"] = 300000;
