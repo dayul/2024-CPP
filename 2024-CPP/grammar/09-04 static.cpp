f79487c85755328440a782b38178da8fe04916d0
dayul <dyulchoo@naver.com>
2024-10-15 15:31:23 +0900

    09-04 static

---
+++ 2024-CPP/grammar/09-04 static.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class SW2_2 {
+public:
+	SW2_2(string name) : name_(name)
+	{
+		num_++;
+	}
+	static string get_teacher(void) { return teacher_; }
+	static int get_num(void) { return num_; }
+	string get_name(void) { return name_; }
+private:
+	static string teacher_;	
+	static int num_;
+	string name_;
+};
+string SW2_2::teacher_ = "권지웅";
+int SW2_2::num_ = 0;
+
+
+int main(void) {
+	SW2_2* da = new SW2_2("다율");
+	cout << "인원 수 : " << da->get_num() << endl;
+
+	SW2_2* ji = new SW2_2("지수");
+	cout << "인원 수 : " << ji->get_num() << endl;
+
+	delete ji;
+	delete da;
+
+	return 0;
+}
