f184f994b2e1eaf76d4de6228fac19179c6f6a88
dayul <dyulchoo@naver.com>
2024-10-15 14:28:57 +0900

    범위기반 for문 참고

--- 2024-CPP/grammar/09-02 범위기반for문.cpp
+++ 2024-CPP/grammar/09-02 범위기반for문.cpp
@@ -7,8 +7,10 @@ int main() {
 
 	vector<int> v = {1, 2, 3, 4, 5};
 
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << endl;
+	// *(포인터)를 못 씀 대신, &(참조) 가능
+	// value는 지역변수이기 때문에 v의 데이터는 변화 X
+	for (int value : v) {
+		cout << value << endl;
 	}
 
 	return 0;
