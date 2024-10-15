cbe0ce21303781fe9feecdc8ddbc3a74891fab07
dayul <dyulchoo@naver.com>
2024-10-15 14:20:26 +0900

    09-02 전통적인 반복문

---
+++ 2024-CPP/grammar/09-02 범위기반for문.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector<int> v = {1, 2, 3, 4, 5};
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
+	return 0;
+}
