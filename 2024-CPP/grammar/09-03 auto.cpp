19a4c1d9f263805b2273225fce4d714049f67f91
dayul <dyulchoo@naver.com>
2024-10-15 14:38:57 +0900

    09-03 auto

---
+++ 2024-CPP/grammar/09-03 auto.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	vector<string> v = {"롯데관광", "동화면세점", "제주드림타워"};
+	
+	// string이라는 자료형이 명확하기 때문에 auto
+	for (auto value : v) {
+		cout << value << endl;
+	}
+
+	// auto a; (error)
+	auto a = 2;			// 가능
+
+
+	return 0;
+}
