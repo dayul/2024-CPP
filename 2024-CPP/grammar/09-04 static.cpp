4790bf959c7102b2c6126db4660b550fa40260c2
dayul <dyulchoo@naver.com>
2024-10-17 10:49:05 +0900

    객체를 생성할 이유는 없다

--- 2024-CPP/grammar/09-04 static.cpp
+++ 2024-CPP/grammar/09-04 static.cpp
@@ -22,7 +22,7 @@ int SW2_2::num_ = 0;
 
 int main(void) {
 	SW2_2* da = new SW2_2("다율");
-	cout << "인원 수 : " << da->get_num() << endl;
+	cout << "인원 수 : " << SW2_2::get_num() << endl;		// 논리적으로는 클래스 이름을 붙이는 게 맞음
 
 	SW2_2* ji = new SW2_2("지수");
 	cout << "인원 수 : " << ji->get_num() << endl;
